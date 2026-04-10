ca44c3385cdabf52eaeb65ec98858d1b3fd565d0
Indrajit Bag <ibag7764@gmail.com>
2026-04-11 00:59:08 +0530

    Update node.c

--- src/node.c
+++ src/node.c
@@ -38,3 +38,32 @@ void get_my_lan_ip(char *buffer) {
     }
     close(sock);
 }
+// ================= GOSSIP SENDER =================
+// Broadcast load to network
+void* shout_load(void* arg) {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    int opt = 1;
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt));
+
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT_UDP);
+    addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    while(1) {
+        double load[1];
+        getloadavg(load, 1);
+
+        float true_load = load[0] / get_nprocs();
+
+        char msg[50];
+        snprintf(msg, sizeof(msg), "LOAD:%f", true_load);
+
+        sendto(sock, msg, strlen(msg), 0,
+               (struct sockaddr*)&addr, sizeof(addr));
+
+        sleep(3);
+    }
+}
+
